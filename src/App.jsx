f3a71463b0eb3403ecb6bc2f735745cc28f03197
Berta Ruiz <bruizpalenzuela@gmail.com>
2023-08-19 20:17:56 +0200

    First version

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,79 @@ import React from "react";
 import { useState } from "react";
 const App = () => {
   const [inputValue, setInputValue] = useState("");
-  const [listTasks, setLisTasks] = useState([]);
+  const [listTasks, setListTasks] = useState([]);
+  const [isOver, setIsOver] = useState(null);
 
-  const handleAddTask = () => {
-    if (inputValue !== "") {
-      setLisTasks([...listTasks, inputValue]);
+  const handleAddTask = (e) => {
+    if (e.key === "Enter" && inputValue !== "") {
+      setListTasks([...listTasks, inputValue]);
+      setInputValue("");
     }
   };
 
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleDeleteTask = (indexToDelete) => {
+    setListTasks(listTasks.filter((task, index) => index !== indexToDelete));
+  };
+
+  const handleMouseEnter = (index) => {
+    setIsOver(index);
+  };
+  const handleMouseLeaves = (index) => {
+    setIsOver(null);
+  };
+
+  const handleEditTask = (event, indexToEdit) => {
+    const updatedTasks = [...listTasks];
+    updatedTasks[indexToEdit] = event.target.value;
+    setListTasks(updatedTasks);
+  };
+  {
+    console.log(listTasks);
+  }
   return (
     <>
-      <div className="containerCenter  d-flex flex-column justify-content-center align-items-center rounded">
-        <p>{`ToDoList :)`} </p>
+      <div className="containerCenter flex-column justify-content-center align-items-center rounded">
+        <h1 className="text-center">{`ToDoList :)`} </h1>
         <ul className="list-group">
           <li className="list-group-item d-inline-flex">
             <input
               className="form-control me-2"
-              placeholder="New task"
+              placeholder="New task (press Enter to add)"
               value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-            ></input>
-            <button className="btn btn-secondary" onClick={handleAddTask}>
-              +
-            </button>
-            {console.log(listTasks)}
+              onChange={handleInputChange}
+              onKeyDown={handleAddTask}
+            />
           </li>
-          {listTasks.map((task, index) => {
-            <li className="list-group-item d-inline-flex" key={index}>
-              {task}
-              <input
-                className="form-control me-2"
-                placeholder="New task"
-                value={listTasks[index]}
-                onChange={(e) => setInputValue(e.target.value)}
-              ></input>
-              {console.log(inputValue)}
-              <button className="btn btn-secondary" onClick={handleAddTask}>
-                +
-              </button>
-            </li>;
-          })}
+          {listTasks.map((task, index) => (
+            <div
+              className=""
+              key={index}
+              onMouseOver={() => handleMouseEnter(index)}
+              onMouseLeave={() => handleMouseLeaves(index)}
+            >
+              <li className="list-group-item d-flex">
+                <input
+                  className="form-control me-2"
+                  // placeholder="New task (press Enter to add)"
+                  value={listTasks[index]}
+                  onChange={(e) => handleEditTask(e, index)}
+                ></input>
+                {console.log(isOver)}
+                {isOver === index && (
+                  <button
+                    className="trashButton btn btn-danger d-flex justify-content-center align-items-center"
+                    onClick={() => handleDeleteTask(index)}
+                  >
+                    <span className=" fa-regular fa-trash-can"></span>
+                  </button>
+                )}
+              </li>
+            </div>
+          ))}
         </ul>
       </div>
     </>
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Container = ({ children }) => {
   return (
     <>
-      <div className="containerCenter  d-flex flex-column justify-content-center align-items-center rounded">
+      <div className="containerCenter d-flex flex-column justify-content-center align-items-center rounded">
         <p>{`ToDoList :)`} </p>
         <div>{children}</div>
       </div>
